a99acb5cb63d0ee127870a1412ea53b3b5b0ebf2
𝐊𝐈𝐍𝐆 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 𝐁𝐎𝐓 <164044359+sormishourovss@users.noreply.github.com>
2024-11-30 18:36:50 +0300

    Update Fahim.2.js

--- scripts/commands/Fahim.2.js
+++ scripts/commands/Fahim.2.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("Bot er malik ke")==0 || (event.body.indexOf("Bot kar")==0 || (event.body.indexOf("Bot tor boss ke")==0 || (event.body.indexOf("I love you")==0)))) {
 		var msg = {
-				body: " আমার বস ফাহিম ইসলাম কিছু বলে আমার বসের ইনবক্সে এ জা Facebook ling : https://www.facebook.com/profile.php?id=100046430705172:))"
+				body: " আমার বস 𝐊𝐢𝐧𝐠_𝐒𝐡𝐨𝐮𝐫𝐨𝐯  কিছু বলে আমার বসের ইনবক্সে এ জা Facebook ling :https://www.facebook.com/broken.shourov.ss? ))"
     }
 			api.sendMessage(msg, threadID, messageID);
 		}
