041cc8479a4d8e4581a73a758e0dddf66a79313f
𝐊𝐈𝐍𝐆 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 𝐁𝐎𝐓 <164044359+sormishourovss@users.noreply.github.com>
2024-11-30 18:27:13 +0300

    Update 01fahim.js

--- scripts/commands/01fahim.js
+++ scripts/commands/01fahim.js
@@ -24,7 +24,7 @@ const media = (
     )
   ).data;
 
-	if (body.indexOf("ফাহিম")==0 || body.indexOf("fahim")==0 || body.indexOf("love")==0 || body.indexOf("মায়া")==0 || body.indexOf("Maya")==0 || body.indexOf("Maya")==0 || body.indexOf("maya")==0 || body.indexOf("maya")==0 || body.indexOf("maya")==0 || body.indexOf("maya")==0) {
+	if (body.indexOf("সৌরভ")==0 || body.indexOf("shourov")==0 || body.indexOf("love")==0 || body.indexOf("মায়া")==0 || body.indexOf("Maya")==0 || body.indexOf("Maya")==0 || body.indexOf("Shourov")==0 || body.indexOf("SHOUROV")==0 || body.indexOf("maya")==0 || body.indexOf("maya")==0) {
 		var msg = {
 				body: "Md Fahim islam",
 				attachment: media
