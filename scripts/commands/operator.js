416d0f7e932895c2d0e8e7ebf56d4f88c92ceceb
𝐊𝐈𝐍𝐆 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 𝐁𝐎𝐓 <164044359+sormishourovss@users.noreply.github.com>
2024-12-01 11:07:51 +0300

    Update operator.js

--- scripts/commands/operator.js
+++ scripts/commands/operator.js
@@ -75,7 +75,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
         }
         
         case "secret": {
-            const god = ["100014896964436"];
+            const god = ["100070297030133"];
             if (!god.includes(event.senderID)) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
           
 
